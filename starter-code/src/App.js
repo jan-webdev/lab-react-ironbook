855b3c35263f8b68dfd5a787541975e548d1b39f
Jan <jan.leon.voelker@gmail.com>
2020-12-03 01:25:42 +0100

    iteration 2 done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import users from "./users.json/users";
+import "./App.css";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+  userList: users,
+  searchValue: '',
+  student: '',
+  teacher: '',
+  campus:''
+  }
+  // methods go here 
+  
+  handleSubmit = event => {
+    event.preventDefault();
+}
+handleSearchChange = event => {
+  console.log(event.target.value);
+  console.log("this is submit")
+  console.log(this.state.userList)
+  let searchFiltered = users.filter( user => 
+    user.lastName.toLowerCase().includes(event.target.value.toLowerCase())
+  )
+  console.log("searchFiltered", searchFiltered);
+  this.setState((state, props) => ({
+    userList: searchFiltered,
+    searchValue: event.target.value
+  }))
+}
+ 
+  render() {
+     const filtered = this.state.userList;
+     console.log(this.state.userList);
+    return (
+      <div className="App">
+        <h1>IronBook</h1>
+        <form onSubmit={this.handleSubmit}>
+        <label htmlFor="search">Search: </label>
+          <input 
+        type="text"
+        name="search"
+        id="search"
+        value={this.state.searchValue}
+        onChange={this.handleSearchChange}
+          />
+          <button type="submit">Search</button>
+        </form>
+        <table>
+          <thead>
+            <tr>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Campus</th>
+              <th>Role</th>
+              <th>Links</th>
+            </tr>
+          </thead>
+          <tbody>
+            {filtered.map((user) => {
+              return (
+                <tr>
+                  <td>{user.firstName}</td>
+                  <td>{user.lastName}</td>
+                  <td>{user.campus}</td>
+                  <td>{user.role}</td>
+                  <td>{user.linkedin}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
 
 export default App;
